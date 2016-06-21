23922b4ed7edf6547ded96bac31211f9ee4efe4f
Anna Lewis <anna.frances.lewis@gmail.com>
2016-06-20 18:05:39 -0600

    updating some things, but still have a lot of changes to come

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 search = function search() {
+
+  // $("#button").click(function(){
+  //   var userInput = document.getElementById("userInput").value
+  //   $.get("http://annnfrann:banana@api.repo.nypl.org/api/v1/items/search?q=cats&publicDomainOnly=true", function(data, status){
+  //     console.log("data: ",  data)
+  //   })
+  // })
+
+  // $("#button").click(function(){
+  //   var userInput = document.getElementById("userInput").value
+  //   $.get("http://api.repo.nypl.org/api/v1/items/search.json?q=" + userInput, function(data, status){
+  //     console.log("data: ",  data)
+  //   })
+  // })
+
   $("#button").click(function(){
     var userInput = document.getElementById("userInput").value
-    $.get("http://loc.gov/pictures/search/?q=" + userInput + "&fo=json", function(data, status){
-      console.log("data: ",  data)
-    })
-  })
+    $.ajax({
+      url: "http://annnfrann:banana@api.repo.nypl.org/api/v1/items/search?q=cats&publicDomainOnly=true",
+      headers: {
+        "Authorization": "Token token=wscle06au2nzk1ho"
+      },
+    success: function(result){
+      console.log(result);
+      alert("yay")
+    }});
+  });
+
 }
-
-
-
-
-$(document).ready(function(){
-  search();
-})
+search()
+// curl "http://api.repo.nypl.org/api/v1/items/search?q=cats&publicDomainOnly=true" -H 'Authorization: Token token="wscle06au2nzk1ho"'
 
 // http://loc.gov/pictures/search/?q=<query>&fo=json replace "<query" with the search term
 
